8a69d7b4a18e82ec78485d324fb4d6a7d8f682cc
mitch <mander1122@gmail.com>
2024-02-28 22:17:56 -0500

    automation v1

--- sway/lock_screen.sh
+++ sway/lock_screen.sh
@@ -6,11 +6,11 @@
 
 # Times the screen off and puts it to background
 swayidle \
-    timeout 10 'swaymsg "output * dpms off"' \
+    timeout 60 'swaymsg "output * dpms off"' \
     resume 'swaymsg "output * dpms on"' &
 
 # Locks the screen immediately
-swaylock -c 550000
+swaylock -i ~/.config/sway/wallpaper.png
 
 # Kills last background task so idle timer doesn't keep running
 kill %%
