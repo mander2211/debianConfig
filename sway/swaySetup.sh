4846ba18a648e9f43c763d057deba1cd71230aee
mitch <mander1122@gmail.com>
2024-03-01 11:11:06 -0500

    comments and org

--- sway/swaySetup.sh
+++ sway/swaySetup.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
+#Directories
 mkdir -p ~/.config/sway ~/.config/waybar ~/.config/wofi ~/.config/foot ~/Downloads ~/Documents
+
+#Sway Files
 cp audio.sh config exit.sh lock_screen.sh clamshell.sh ~/.config/sway/
-cp ../waybar/* ~/.config/waybar/
-cp ../wofi/* ~/.config/wofi/
 cp ../wallpaper.png ~/.config/sway/
-cp ../foot/foot.ini ~/.config/foot/
-cp ../.bash_aliases ~/
-cp ../bootpaper.png /boot/grub
-update-grub
 chmod +x ~/.config/sway/audio.sh
 chmod +x ~/.config/sway/exit.sh
 chmod +x ~/.config/sway/lock_screen.sh
 chmod +x ~/.config/sway/clamshell.sh
 
+#Waybar Files
+cp ../waybar/* ~/.config/waybar/
 
-##apps
+#Wofi Files
+cp ../wofi/* ~/.config/wofi/
 
+#Foot Files
+cp ../foot/foot.ini ~/.config/foot/
+cp ../.bash_aliases ~/
+
+#Grub Config
+cp ../bootpaper.png /boot/grub
+update-grub
+
+#Apps
 apt install foot btop pavucontrol rxvt tldr vlc sway swaybg swayidle swayimg swaylock waybar wofi fonts-font-awesome xdg-desktop-portal-wlr xwayland vim neofetch sway-backgrounds flatpak pulseaudio python3 nodejs npm gcc build-essential gdb gimp htop timeshift ark thunar -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flatpakrepo
 flatpak install flathub org.mozilla.firefox -y -y
