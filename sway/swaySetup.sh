20e948c9deb296933ee38967e72adea48aef37a3
mitch <mander1122@gmail.com>
2024-02-29 20:02:54 -0500

    wayfar fixes

--- sway/swaySetup.sh
+++ sway/swaySetup.sh
@@ -7,6 +7,8 @@ cp ../wofi/* ~/.config/wofi/
 cp ../wallpaper.png ~/.config/sway/
 cp ../foot/foot.ini ~/.config/foot/
 cp ../.bash_aliases ~/
+cp ../bootpaper.png /boot/grub
+update-grub
 chmod +x ~/.config/sway/audio.sh
 chmod +x ~/.config/sway/exit.sh
 chmod +x ~/.config/sway/lock_screen.sh
@@ -14,7 +16,7 @@ chmod +x ~/.config/sway/lock_screen.sh
 
 ##apps
 
-apt install foot firefox-esr vlc sway swaybg swayidle swayimg swaylock waybar wofi fonts-font-awesome xdg-desktop-portal-wlr xwayland vim neofetch sway-backgrounds flatpak pulseaudio python3 nodejs npm gcc build-essential gdb gimp htop timeshift ark thunar -y
+apt install foot btop pavucontrol rxvt tldr firefox-esr vlc sway swaybg swayidle swayimg swaylock waybar wofi fonts-font-awesome xdg-desktop-portal-wlr xwayland vim neofetch sway-backgrounds flatpak pulseaudio python3 nodejs npm gcc build-essential gdb gimp htop timeshift ark thunar -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flatpakrepo
 ##flatpak install flathub org.mozilla.firefox -y -y
 ##flatpak install flathub org.videolan.VLC -y -y
