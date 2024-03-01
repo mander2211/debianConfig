e60f6fc712ea850206995ec8c62da675854e71fa
mitch <mander1122@gmail.com>
2024-03-01 11:02:11 -0500

    just use flatpak firefox... looks better on hidpi

--- sway/swaySetup.sh
+++ sway/swaySetup.sh
@@ -16,12 +16,11 @@ chmod +x ~/.config/sway/lock_screen.sh
 
 ##apps
 
-apt install foot btop pavucontrol rxvt tldr firefox-esr vlc sway swaybg swayidle swayimg swaylock waybar wofi fonts-font-awesome xdg-desktop-portal-wlr xwayland vim neofetch sway-backgrounds flatpak pulseaudio python3 nodejs npm gcc build-essential gdb gimp htop timeshift ark thunar -y
+apt install foot btop pavucontrol rxvt tldr vlc sway swaybg swayidle swayimg swaylock waybar wofi fonts-font-awesome xdg-desktop-portal-wlr xwayland vim neofetch sway-backgrounds flatpak pulseaudio python3 nodejs npm gcc build-essential gdb gimp htop timeshift ark thunar -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flatpakrepo
-##flatpak install flathub org.mozilla.firefox -y -y
+flatpak install flathub org.mozilla.firefox -y -y
 ##flatpak install flathub org.videolan.VLC -y -y
 
-echo "In ~/.Profiles add "export MOZ_ENABLE_WAYLAND=1"
 echo "Complete..."
 
 
