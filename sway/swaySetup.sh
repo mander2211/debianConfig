836b1f1a2cf199ff9a3788b448a4706c329ff470
Mitch <mander1122@gmail.com>
2025-01-01 17:45:08 -0500

    changed to kitty/added progs

--- sway/swaySetup.sh
+++ sway/swaySetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Directories
-mkdir -p ~/.config/sway ~/.config/waybar ~/.config/wofi ~/.config/foot ~/Downloads ~/Documents
+mkdir -p ~/.config/sway ~/.config/waybar ~/.config/wofi ~/.config/kitty ~/Downloads ~/Documents
 
 #Sway Files
 cp audio.sh config exit.sh lock_screen.sh clamshell.sh ~/.config/sway/
@@ -18,19 +18,21 @@ cp ../waybar/* ~/.config/waybar/
 cp ../wofi/* ~/.config/wofi/
 
 #Foot Files
-cp ../foot/foot.ini ~/.config/foot/
-cp ../.bash_aliases ~/
+##cp ../foot/foot.ini ~/.config/foot/
+##cp ../.bash_aliases ~/
 
 #Grub Config
 cp ../bootpaper.png /boot/grub
 update-grub
 
 #Apps
-apt install foot btop pavucontrol rxvt tldr vlc sway swaybg swayidle swayimg swaylock waybar wofi fonts-font-awesome xdg-desktop-portal-wlr xwayland vim neofetch sway-backgrounds flatpak pulseaudio python3 nodejs npm gcc build-essential gdb gimp htop timeshift ark thunar -y
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flatpakrepo
-flatpak install flathub org.mozilla.firefox -y -y
+apt install kitty btop pavucontrol rxvt tldr vlc sway fish swaybg swayidle swayimg swaylock waybar wofi fonts-font-awesome xdg-desktop-portal-wlr xwayland neovim neofetch sway-backgrounds flatpak pulseaudio python3 nodejs npm gcc build-essential gdb gimp timeshift ark thunar wget curl tmux ssh rsync network-manager firefox-esr man exa fzf git -y
+##flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flatpakrepo
+##flatpak install flathub org.mozilla.firefox -y -y
 ##flatpak install flathub org.videolan.VLC -y -y
 
+##Kitty config
+cp ../kitty.conf ~/.config/kitty/
 echo "Complete..."
 
 
